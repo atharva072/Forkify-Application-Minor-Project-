8f8ca23e7fc2b992f5a0256b469038fdea7307e9
Atharva Sadavarte <firetornado13@gmail.com>
2021-10-29 15:18:21 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'aabb1fc9-640b-4187-8ac8-e31555eef6ad';
+export const MODAL_CLOSE_SEC = 2.5;
